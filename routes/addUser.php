4a07537756dcf5bd790c851751421b0c786aecdb
Austin Nielsen <austinpnielsen@hotmail.com>
2018-01-26 12:19:38 -0600

    Connecting to DB 1.0.21

--- routes/addUser.php
+++ routes/addUser.php
@@ -1,14 +1,14 @@
 <?php
-	$servername = "localhost";
-	$username = "anielsen";
-	$password = "VikingS42";
-	$dbname = 'testDB';
-	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	// Check connection
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	} 
+	// $servername = "localhost";
+	// $username = "anielsen";
+	// $password = "VikingS42";
+	// $dbname = 'testDB';
+	// // Create connection
+	// $conn = new mysqli($servername, $username, $password, $dbname);
+	// // Check connection
+	// if ($conn->connect_error) {
+	//     die("Connection failed: " . $conn->connect_error);
+	// } 
 
 	echo $_POST['username'];
 	echo $_POST['password'];
