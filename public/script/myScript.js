0d430315629198a56a14be5bfcf9f184b713b55f
Austin Nielsen <austinpnielsen@hotmail.com>
2018-01-26 16:06:30 -0600

    Fixing Login tab for Smaller devices

--- public/script/myScript.js
+++ public/script/myScript.js
@@ -1,3 +1,10 @@
+$(window).onload(function() {
+	var viewportWidth = $(window).width();
+	if (viewportWidth <= 992) {
+		$("#loginNavID").removeClass("navbar-toggler-right");
+	}
+});
+
 $(window).resize(function() {
 	var viewportWidth = $(window).width();
 	if (viewportWidth <= 992) {
