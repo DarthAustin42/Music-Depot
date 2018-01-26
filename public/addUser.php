42b5cb51a9cf101e1c0af2baed8b701e311851b4
Austin Nielsen <austinpnielsen@hotmail.com>
2018-01-26 14:10:22 -0600

    Connecting to DB 1.0.38

--- public/addUser.php
+++ public/addUser.php
@@ -1,30 +1,28 @@
 <?php
-	$app = require __DIR__.'/../bootstrap/app.php';
-	$app->post('addUser', function (Illuminate\Http\Request $request) {
-		$USERNAME = $_POST["username"];
-		$PASSWORD = $_POST["password"];
+	
+	$USERNAME = $_POST["username"];
+	$PASSWORD = $_POST["password"];
 
 
-		$servername = "localhost";
-		$username = "anielsen";
-		$password = "VikingS42";
-		$dbname = 'testDB';
-		// Create connection
-		$conn = new mysqli($servername, $username, $password, $dbname);
-		// Check connection
-		if ($conn->connect_error) {
-		    die("Connection failed: " . $conn->connect_error);
-		} 
+	$servername = "localhost";
+	$username = "anielsen";
+	$password = "VikingS42";
+	$dbname = 'testDB';
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	} 
 
-		// Create database
-		$sql = "INSERT INTO users VALUES ('$USERNAME', '$PASSWORD')";
-		if ($conn->query($sql) === TRUE) {
-			echo "New record created successfully";
-		} else {
-		    echo "Error creating database2: " . $conn->error;
-		}
+	// Create database
+	$sql = "INSERT INTO users VALUES ('$USERNAME', '$PASSWORD')";
+	if ($conn->query($sql) === TRUE) {
+		echo "New record created successfully";
+	} else {
+	    echo "Error creating database2: " . $conn->error;
+	}
 
-		$conn->close();
-		return;
-	})
+	$conn->close();
+	return;
 ?>
