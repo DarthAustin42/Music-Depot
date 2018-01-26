a763a969c553086fb39498a41a67198e05c48f89
Austin Nielsen <austinpnielsen@hotmail.com>
2018-01-26 16:46:31 -0600

    Changing Login Item

--- public/script/myScript.js
+++ public/script/myScript.js
@@ -3,6 +3,10 @@ function moveLogin() {
 	if (viewportWidth <= 992) {
 		$("#loginNavID").removeClass("navbar-toggler-right");
 	}
+
+	if($("#navbarDropdownMenuLink").text() != "Login/Sign Up") {
+		$("#navbarDropdownMenuLink").addClass("hidden");
+	}
 }
 
 $(window).resize(function() {
