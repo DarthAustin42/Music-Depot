dbc327faf620fcfc9f919f5a5b06fa577ca5c30b
Austin Nielsen <austinpnielsen@hotmail.com>
2018-01-26 12:41:32 -0600

    Connecting to DB 1.0.30

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,29 @@
 
 $router->get('/', function () use ($router) {
     include('index.html');
+    $USERNAME = $_POST('username');
+	$PASSWORD =  $_POST('password');
+
+
+	$servername = "localhost";
+	$username = "anielsen";
+	$password = "VikingS42";
+	$dbname = 'testDB';
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	} 
+
+	// Create database
+	$sql = "INSERT INTO users VALUES ('$USERNAME', '$PASSWORD')";
+	if ($conn->query($sql) === TRUE) {
+		echo "New record created successfully";
+	} else {
+	    echo "Error creating database2: " . $conn->error;
+	}
+
+	$conn->close();
     return ;
 });
