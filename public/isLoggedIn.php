ddef3ee7d85d43e819419f0588184cddfcb9d40e
Austin Nielsen <austinpnielsen@hotmail.com>
2018-01-26 16:33:11 -0600

    Changing Login Item

--- public/isLoggedIn.php
+++ public/isLoggedIn.php
@@ -1,9 +1,10 @@
 <?php
 	session_start();
 
-	if(isset($_SESSION["username"])) {
-    	return $_SESSION["username"];
-    } else {
-    	return "Login/Sign Up";
-    }
+	echo "hi, " . $_SESSION["username"];
+	// if(isset($_SESSION["username"])) {
+ //    	return $_SESSION["username"];
+ //    } else {
+ //    	return "Login/Sign Up";
+ //    }
 ?>
