916114e46cd6bec060ec38709f452e921def9179
Austin Nielsen <austinpnielsen@hotmail.com>
2018-01-26 12:17:09 -0600

    Connecting to DB 1.0.19

---
+++ public/addUser.php
@@ -0,0 +1,22 @@
+<?php
+	$servername = "localhost";
+	$username = "anielsen";
+	$password = "VikingS42";
+	$dbname = 'testDB';
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	} 
+
+	// Create database
+	$sql = "INSERT INTO users VALUES ($_POST['username'], $_POST['password'])";
+	if ($conn->query($sql) === TRUE) {
+		echo "New record created successfully";
+	} else {
+	    echo "Error creating database2: " . $conn->error;
+	}
+
+	$conn->close();
+?>
