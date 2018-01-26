22fad6997bb184d3362c52c1a0b17a3aa7ca56a3
Austin Nielsen <austinpnielsen@hotmail.com>
2018-01-26 11:06:05 -0600

    Fixing Navbar 1.4.49

--- public/script/myScript.js
+++ public/script/myScript.js
@@ -1,5 +1,6 @@
 $(window).resize(function() {
 	var viewportWidth = $(window).width();
+	console.log(viewportWidth);
 	if (viewportWidth < 600) {
 		$("#loginNavID").removeClass("navbar-toggler-right");
 	}
