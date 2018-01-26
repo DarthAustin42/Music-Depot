5a12e5bffa1d0519efbb08802d5a948b0e7241f0
Austin Nielsen <austinpnielsen@hotmail.com>
2018-01-26 14:45:26 -0600

    Connecting to DB 1.0.57

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ $router->get('/', function () use ($router) {
     if(isset($_SESSION['username'])) {
     	echo "Hello, " . $_POST["username"];
     } else {
-    	echo "No";
+    	echo "Hi, " . $_SESSION['username'];
     }
     return ;
 });
