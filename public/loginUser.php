8e7cc30f40984df60c247cc33c2f72cdca62f52c
Austin Nielsen <austinpnielsen@hotmail.com>
2018-01-29 14:57:01 -0600

    Changing DB Settings

--- public/loginUser.php
+++ public/loginUser.php
@@ -4,25 +4,11 @@
 	$USERNAME = $_POST["username"];
 	$PASSWORD = $_POST["password"];
 
-
-	$servername = "localhost";
-	$username = "anielsen";
-	$password = "VikingS42";
-	$dbname = 'testDB';
-	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	// Check connection
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	} 
-
-	// Create database
-	//$sql = "INSERT INTO users VALUES ('$USERNAME', '$PASSWORD')";
 	$sql = "SELECT * FROM users";
-	if ($allUsers = mysqli_query($conn, $sql)) {
+	if ($result = app('db')->select($sql)) {
 		$foundUser = false;
-		while($row = mysqli_fetch_array($allUsers)) {
-			if($row['username'] === $USERNAME && $row['password'] === $PASSWORD) {
+		while($result as $row) {
+			if($row->username === $USERNAME && $row->password === $PASSWORD) {
 				$_SESSION["username"] = $_POST["username"];
 				header("LOCATION: /");
 				// echo "Hello there, " . $_SESSION['username'];
@@ -34,14 +20,12 @@
 			echo "There is no user with that information";
 		}
 
-		mysqli_free_result($allUsers);
 		//echo $allUsers;
 		//$_SESSION['username'] = $_POST["username"];
 		//header("LOCATION: /index.php");
 	} else {
-	    echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+	    echo "ERROR: Could not able to execute $sql. ";
 	}
 
-	mysqli_close($conn);
 	return;
 ?>
