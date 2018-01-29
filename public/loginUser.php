06cde6ef3c94e35cc866ea5ca59ab727db304bf3
Austin Nielsen <austinpnielsen@hotmail.com>
2018-01-29 15:00:32 -0600

    Changing DB Settings

--- public/loginUser.php
+++ public/loginUser.php
@@ -4,34 +4,39 @@
 	$USERNAME = $_POST["username"];
 	$PASSWORD = $_POST["password"];
 
-	$sql = "SELECT * FROM users";
-	if ($result = app('db')->select($sql)) {
-		$foundUser = false;
-		while($result as $row) {
-			$theUs = $row->username;
-			$thePass = $row->password;
-			echo $theUs . "  ::  " . $thePass;
-			if($theUs === $USERNAME && $thePass === $PASSWORD) {
-				$_SESSION["username"] = $_POST["username"];
-				header("LOCATION: /");
-				// echo "Hello there, " . $_SESSION['username'];
-				// $foundUser = true;
-				break;
-			}
-		}
-		if(!$foundUser) {
-			echo "There is no user with that information";
-		}
-		else {
-			"hi";
-		}
-
-		//echo $allUsers;
-		//$_SESSION['username'] = $_POST["username"];
-		//header("LOCATION: /index.php");
-	} else {
-	    echo "ERROR: Could not able to execute $sql. ";
+	$result = app('db')->select("SELECT * FROM users");
+	foreach ($result as $user) {
+		echo $user->username;
 	}
+
+	// $sql = "SELECT * FROM users";
+	// if ($result = app('db')->select($sql)) {
+	// 	$foundUser = false;
+	// 	while($result as $row) {
+	// 		$theUs = $row->username;
+	// 		$thePass = $row->password;
+	// 		echo $theUs . "  ::  " . $thePass;
+	// 		if($theUs === $USERNAME && $thePass === $PASSWORD) {
+	// 			$_SESSION["username"] = $_POST["username"];
+	// 			header("LOCATION: /");
+	// 			// echo "Hello there, " . $_SESSION['username'];
+	// 			// $foundUser = true;
+	// 			break;
+	// 		}
+	// 	}
+	// 	if(!$foundUser) {
+	// 		echo "There is no user with that information";
+	// 	}
+	// 	else {
+	// 		"hi";
+	// 	}
+
+	// 	//echo $allUsers;
+	// 	//$_SESSION['username'] = $_POST["username"];
+	// 	//header("LOCATION: /index.php");
+	// } else {
+	//     echo "ERROR: Could not able to execute $sql. ";
+	//}
 
 	return;
 ?>
